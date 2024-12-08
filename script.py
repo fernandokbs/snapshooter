848ccd20498c35ed26a8ece2f53ddd49e58d42a4
Fernando <fernando93d@gmail.com>
2024-12-08 10:13:16 -0600

    Run backups

--- script.py
+++ script.py
@@ -15,7 +15,7 @@ class BackupService:
         if not self.token:
             raise Exception("Key not found")
 
-    def jobs(self):
+    def job_ids(self):
         response = requests.get(f'{self.BASE_URL}/jobs', headers=self._headers())
         jobs_data = response.json()
         job_ids = []
@@ -31,11 +31,15 @@ class BackupService:
             raise Exception(f"Failed to fetch data: {response.status_code}")
 
     def run_backups(self):
-        job_ids = self.jobs()
+        job_ids = self.job_ids()
 
         for job in job_ids:
             job_id = job['id']
-            print(f'{self.BASE_URL}/jobs/{job_id}/backups/start')
+            job_name = job['name']
+            job_url = f'{self.BASE_URL}/jobs/{job_id}/backups/start'
+            print(f'Running backup for {job_name} with id: {job_id}')
+
+            response = requests.post(job_url, headers=self._headers(), data=self._data_binary())
 
     def test_backup(self):
         url = f'{self.BASE_URL}/jobs/{self.job_id}/backups/start'
@@ -60,7 +64,7 @@ class BackupService:
             self._download_file(f'{self.BASE_DESTINATION_PATH}r/{file_name}', value)
 
     def call(self):
-        self.download_backup()
+        self.run_backups()
 
     def _download_file(self, file_destination_path, url_file):
         with requests.get(url_file, stream=True)  as response:
